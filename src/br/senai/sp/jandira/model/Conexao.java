a58eaf0bf6d3b6a1e855b29ac0f863f721c31d59
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-11-22 11:57:49 -0300

    Crud

---
+++ src/br/senai/sp/jandira/model/Conexao.java
@@ -0,0 +1,36 @@
+package br.senai.sp.jandira.model;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private String servidor, banco, usuario, senha;
+    public Connection conexao;
+
+    public Conexao (){
+        this.servidor = "LocalHost";
+        this.banco = "db_empresa";
+        this.usuario = "root";
+        this.senha = "";
+    }
+
+
+    public void connectDriver(){
+        try {
+            this.conexao = DriverManager.getConnection("jdbc:mysql://" +
+                    this.servidor + "/" + this.banco, this.usuario, this.senha);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+
+    public Connection getConnection(){
+        connectDriver();
+        return conexao;
+    }
+
+}
